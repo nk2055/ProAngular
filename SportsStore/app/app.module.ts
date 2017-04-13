001930400313f33630dc2c37372d4918c47a0a9c
Jeong H Park <hello@jeonghyuk.info>
2017-04-12 21:56:36 -0400

    Preparing the Example Application completed p.174

--- SportsStore/app/app.module.ts
+++ SportsStore/app/app.module.ts
@@ -22,6 +22,11 @@ import { StoreFirstGuard } from "./storeFirst.guard";
             path: "checkout", component: CheckoutComponent,
             canActivate: [StoreFirstGuard]
         },
+        {
+            path: "admin",
+            loadChildren: "app/admin/admin.module#AdminModule",
+            canActivate: [StoreFirstGuard]
+        },
         { path: "**", redirectTo: "/store" }
     ])],
     providers: [StoreFirstGuard],
