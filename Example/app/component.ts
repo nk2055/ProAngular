060cc8d8889f44ee7df83e38313380bc69794922
Jeong H Park <hello@jeonghyuk.info>
2017-05-06 18:20:00 -0400

    Working with Forms completed p.324

--- Example/app/component.ts
+++ Example/app/component.ts
@@ -1,17 +1,21 @@
 import { ApplicationRef, Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 import { Model } from "./repository.model";
 import { Product } from "./product.model";
 
 @Component({
     selector: "app",
-    templateUrl: "app/EventBinding.html"
+    templateUrl: "app/Forms.html"
 })
 export class ProductComponent {
     model: Model = new Model();
+    newProduct: Product = new Product();
 
     counter: number = 1;
     targetName: string = "Kayak";
     selectedProduct: string;
+
+    formSubmitted: boolean = false;
 
     constructor(ref: ApplicationRef) {
         (<any>window).appRef = ref;
@@ -49,8 +53,58 @@ export class ProductComponent {
         return product.name == this.selectedProduct;
     }
 
+    addProduct(p: Product) {
+        console.log("New Product: " + this.jsonProduct);
+    }
+
+    getValidationMessages(state: any, thingName?: string) {
+        let thing: string = state.path || thingName;
+        let messages: string[] = [];
+        if (state.errors) {
+            for (let errorName in state.errors) {
+                switch(errorName) {
+                    case "required": {
+                        messages.push(`You must enter a ${thing}`);
+                        break;
+                    }
+                    case "minlength":{
+                        messages.push(`A ${thing} must be at least ${state.errors['minlength'].requiredLength} characters`);
+                        break;
+                    }
+                    case "pattern": {
+                        messages.push(`The ${thing} contains illegal characters`);
+                        break;
+                    }
+                }
+            }
+        }
+        return messages;
+    }
+
+    getFormValidationMessages(form: NgForm): string[] {
+        let messages: string[] = [];
+        Object.keys(form.controls).forEach(key => {
+            this.getValidationMessages(form.controls[key], key).forEach(m => messages.push(m));
+        });
+        return messages;
+    }
+
+    submitForm(form: NgForm) {
+        this.formSubmitted = true;
+        if (form.valid) {
+            this.addProduct(this.newProduct);
+            this.newProduct = new Product();
+            form.reset();
+            this.formSubmitted = false;
+        }
+    }
+
     get nextProduct(): Product {
         return this.model.getProducts().shift();
+    }
+
+    get jsonProduct() {
+        return JSON.stringify(this.newProduct);
     }
 }
 
