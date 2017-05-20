c09fc3ff20b731911873381a242bc95e417baab5
Jeong H Park <hello@jeonghyuk.info>
2017-05-20 18:47:09 -0400

    Preparing Example Project completed p.340

--- Example/app/component.ts
+++ Example/app/component.ts
@@ -6,17 +6,12 @@ import { ProductFormGroup } from "./form.model";
 
 @Component({
     selector: "app",
-    templateUrl: "app/Forms-Model.html"
+    templateUrl: "app/template.html"
 })
 export class ProductComponent {
     model: Model = new Model();
-    newProduct: Product = new Product();
     form: ProductFormGroup = new ProductFormGroup();
-
-    counter: number = 1;
-    targetName: string = "Kayak";
-    selectedProduct: string;
-
+    newProduct: Product = new Product();
     formSubmitted: boolean = false;
 
     constructor(ref: ApplicationRef) {
@@ -25,15 +20,7 @@ export class ProductComponent {
 
         console.log((<any>window).model);
     }
-
-    getKey(index: number, product: Product) {
-        return product.id;
-    }
-
-    getProductByPosition(position: number): Product {
-        return this.model.getProducts()[position];
-    }
-
+    
     getProduct(key: number): Product {
         var product = this.model.getProduct(key);
         return product;
@@ -43,52 +30,9 @@ export class ProductComponent {
         return this.model.getProducts();
     }
 
-    getProductCount(): number {
-        return this.getProducts().length;
-    }
-
-    getProductPrice(index: number): number {
-        return Math.floor(this.getProduct(index).price);
-    }
-
-    getSelected(product: Product): boolean {
-        return product.name == this.selectedProduct;
-    }
-
     addProduct(p: Product) {
         console.log("New Product: " + this.jsonProduct);
-    }
-
-    getValidationMessages(state: any, thingName?: string) {
-        let thing: string = state.path || thingName;
-        let messages: string[] = [];
-        if (state.errors) {
-            for (let errorName in state.errors) {
-                switch(errorName) {
-                    case "required": {
-                        messages.push(`You must enter a ${thing}`);
-                        break;
-                    }
-                    case "minlength":{
-                        messages.push(`A ${thing} must be at least ${state.errors['minlength'].requiredLength} characters`);
-                        break;
-                    }
-                    case "pattern": {
-                        messages.push(`The ${thing} contains illegal characters`);
-                        break;
-                    }
-                }
-            }
-        }
-        return messages;
-    }
-
-    getFormValidationMessages(form: NgForm): string[] {
-        let messages: string[] = [];
-        Object.keys(form.controls).forEach(key => {
-            this.getValidationMessages(form.controls[key], key).forEach(m => messages.push(m));
-        });
-        return messages;
+        this.model.saveProduct(p);
     }
 
     submitForm(form: NgForm) {
