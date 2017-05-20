ea692ef5aade99d7acb96c978e5f3810b3340665
Jeong H Park <hello@jeonghyuk.info>
2017-05-20 18:59:00 -0400

    Creating Simple Attribute Directive completed p. 342

--- Example/app/app.module.ts
+++ Example/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { ProductComponent } from "./component";
+import { PaAttrDirective } from "./attr.directive";
 
 @NgModule({
     imports: [BrowserModule, FormsModule, ReactiveFormsModule],
-    declarations: [ProductComponent],
+
+    // declarations declares Directives & Components
+    declarations: [ProductComponent, PaAttrDirective],
+    
     bootstrap: [ProductComponent]
 })
 export class AppModule {}
