f5df21c03182cf226455fffa7fd184119c0e31c6
Jeong H Park <hello@jeonghyuk.info>
2017-05-20 20:14:29 -0400

    Access Application Data in Directive completed p.351

--- Example/app/attr.directive.ts
+++ Example/app/attr.directive.ts
@@ -1,10 +1,28 @@
-import { Directive, ElementRef } from "@angular/core";
+import { Directive, ElementRef, Attribute, Input, SimpleChange } from "@angular/core";
 
 @Directive({
     selector: "[pa-attr]",
 })
 export class PaAttrDirective {
-    constructor(element: ElementRef) {
-        element.nativeElement.classList.add("bg-success");
+    constructor(private element: ElementRef) { }
+
+    @Input("pa-attr")
+    bgClass: string;
+
+    ngOnInit() {
+        this.element.nativeElement.classList.add(this.bgClass || "bg-success");
+    }
+
+    ngOnChanges(changes: {[property: string]: SimpleChange }) {
+        console.log(changes);
+        let change = changes["bgClass"];
+        let classList = this.element.nativeElement.classList;
+
+        if (!change.isFirstChange() && classList.contains(change.previousValue)) {
+            classList.remove(change.previousValue);
+        }
+        if (!classList.contains(change.currentValue)) {
+            classList.add(change.currentValue);
+        }
     }
 }
