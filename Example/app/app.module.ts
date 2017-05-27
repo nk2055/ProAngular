78c6d96b5996e3c7671632f7f8b0b43a9bc0c023
Jeong H Park <hello@jeonghyuk.info>
2017-05-27 12:22:33 -0400

    Creating Two-Way Binding completed p.359

--- Example/app/app.module.ts
+++ Example/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { ProductComponent } from "./component";
 import { PaAttrDirective } from "./attr.directive";
+import { PaModel } from "./twoway.directive";
 
 @NgModule({
     imports: [BrowserModule, FormsModule, ReactiveFormsModule],
 
     // declarations declares Directives & Components
-    declarations: [ProductComponent, PaAttrDirective],
+    declarations: [ProductComponent, PaAttrDirective, PaModel],
     
     bootstrap: [ProductComponent]
 })
