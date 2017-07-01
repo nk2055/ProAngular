6dff231939821abaaecf3f8fb7e506ac39ec5412
Jeong H Park <hello@jeonghyuk.info>
2017-07-01 19:25:14 -0400

    Understanding RESTful Web Services completed p.608

--- ExampleApp/app/model/model.module.ts
+++ ExampleApp/app/model/model.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { HttpModule } from "@angular/http";
-import { StaticDataSource } from "./static.datasource";
+// import { StaticDataSource } from "./static.datasource";
+import { RestDataSource, REST_URL } from "./rest.datasource";
 import { Model } from "./repository.model";
 
 @NgModule({
     imports: [HttpModule],
-    providers: [Model, StaticDataSource]
+    providers: [Model, RestDataSource, { provide: REST_URL, useValue: `http://${location.hostname}:3500/products`}]
 })
 export class ModelModule { }
 
