b802187913b2122b1382267b2c0099a2868b34af
Jeong H Park <hello@jeonghyuk.info>
2017-07-15 17:47:35 -0400

    Delaying Navigation with a Resolver completed p.693

--- ExampleApp/app/app.routing.ts
+++ ExampleApp/app/app.routing.ts
@@ -4,16 +4,24 @@ import { FormComponent } from "./core/form.component";
 import { NotFoundComponent } from "./core/notFound.component";
 import { ProductCountComponent } from "./core/productCount.component";
 import { CategoryCountComponent } from "./core/categoryCount.component";
+import { ModelResolver } from "./model/model.resolver";
 
 const childRoutes: Routes = [
-    { path: "products", component: ProductCountComponent },
-    { path: "categories", component: CategoryCountComponent },
-    { path: "", component: ProductCountComponent }
+    {
+        path: "",
+        children: [
+            { path: "products", component: ProductCountComponent },
+            { path: "categories", component: CategoryCountComponent },
+            { path: "", component: ProductCountComponent }
+        ],
+        resolve: { mode: ModelResolver }
+    }
+    
 ];
 
 const routes: Routes = [
-    { path: "form/:mode/:id", component: FormComponent },
-    { path: "form/:mode", component: FormComponent },
+    { path: "form/:mode/:id", component: FormComponent, resolve: { model: ModelResolver} },
+    { path: "form/:mode", component: FormComponent, resolve: { model: ModelResolver } },
     { path: "table", component: TableComponent, children: childRoutes },
     { path: "table/:category", component: TableComponent, children: childRoutes },
     { path: "", redirectTo: "/table", pathMatch: "full" },
--- ExampleApp/app/model/model.module.ts
+++ ExampleApp/app/model/model.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from "@angular/core";
 import { HttpModule } from "@angular/http";
-// import { StaticDataSource } from "./static.datasource";
 import { RestDataSource, REST_URL } from "./rest.datasource";
 import { Model } from "./repository.model";
+import { ModelResolver } from "./model.resolver";
 
 @NgModule({
     imports: [HttpModule],
-    providers: [Model, RestDataSource, { provide: REST_URL, useValue: `http://${location.hostname}:3500/products`}]
+    providers: [Model, RestDataSource, ModelResolver, { provide: REST_URL, useValue: `http://${location.hostname}:3500/products`}]
 })
 export class ModelModule { }
 
