6cce0d4e8efe68cecdd71d8538537fd059a575a1
Jeong H Park <hello@jeonghyuk.info>
2017-07-16 15:59:19 -0400

    Targeting Named Outlets completed p.720

---
+++ ExampleApp/app/ondemand/first.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "first",
+    template: `<div class="bg-primary p-a-1">First Component</div>`
+})
+export class FirstComponent { }
+
